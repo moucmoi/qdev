ddcce589d9187617c3b3ae3556c3af98451c9573
Noa Fonteny <noatoimoi@gmail.com>
2024-02-01 09:27:10 +0100

    creation style

--- Style.java
+++ Style.java
@@ -1,3 +1,17 @@
-class Style{
-    
+public enum Style {
+    COMÉDIE, TRAGÉDIE, DRAME;
+
+	@Override
+    public String toString() {
+		if (this == COMÉDIE)
+			return "Comédie";
+		
+		else if (this == TRAGÉDIE)
+			return "Tragédie";
+		
+		else {
+			assert this == DRAME;
+			return "Drame";
+		}
+    }
 }

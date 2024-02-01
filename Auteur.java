e6b3e1d4c04a87434624b55d7e7dbfcc9c22d1bd
Noa Fonteny <noatoimoi@gmail.com>
2024-02-01 08:36:56 +0100

    fonction getqualitétrajedie

--- Auteur.java
+++ Auteur.java
@@ -1,6 +1,16 @@
-class Auteur{
+public class Auteur{
     private String Nom;
     private int perfoTraj;
     private int perfoCom;
     private int perfoDrame;
+
+    public Auteur(nom,perfoTraj,perfoCom,perfoDrame){
+        this.Nom=nom;
+        this.perfoTraj=perfoTraj;
+        this.perfoCom=perfoCom;
+        this.perfoDrame=perfoDrame;
+    }
+    public int getQualitéTragédie(){
+        return this.perfoTraj;
+    }
 }

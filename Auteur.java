f43791ee4f5355eda9b8fd333b8677d6c550fed8
Noa Fonteny <noatoimoi@gmail.com>
2024-02-01 09:06:39 +0100

    rectification map

--- Auteur.java
+++ Auteur.java
@@ -3,19 +3,26 @@ import java.util.HashMap;
 
 public class Auteur{
     private String nom;
-    private Map <String, Integer> perfoTraj;
-    private Map <String, Integer> perfoCom;
-    private Map <String, Integer> perfoDrame;
-    public Auteur(String nom){
+    private Map <String, Integer> perf;
+    private Map <String, String> citation;
+    public Auteur(String nom,int trajedie, String cittra, int comedie, String citcom, int drame, String citdram){
         this.nom=nom;
-        this.perfoTraj=new HashMap<>();
-        this.perfoCom=new HashMap<>();
-        this.perfoDrame=new HashMap<>();
+        this.perf=new HashMap<>();
+        this.citation=new HashMap<>();
+        this.citation.put("Trajedie",cittra);
+        this.citation.put("Comedie",citcom);
+        this.citation.put("Drame",citdram);
+        
+        this.perf.put("Trajedie", trajedie);
+        this.perf.put("Comedie", comedie);
+        this.perf.put("Drame", drame);
     }
     
     public Map <String, Integer> getQualitéTragédie(){
         return this.perfoTraj;
     }
+
+    public  getCitationTragédie
 
    @Override
    public String toString(){
